61c3317d93acd0286a1a135349f13551bb3b36b5
leomet07 <leomet07@gmail.com>
2022-01-23 20:43:50 -0500

    About page updates

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,9 +1,26 @@
 <main>
 	<h1>About</h1>
+	<div id="info">
+		<p>
+			The site was made in Svelte and its <a
+				href="https://github.com/leomet07/maker_portfolio"
+				target="_blank">source code</a
+			> is open source and on GitHub.
+		</p>
+		<p>
+			Made with &lt;3 by <a
+				href="https://github.com/leomet07/"
+				target="_blank">Lenny</a
+			>.
+		</p>
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
+	}
+	#info p {
+		font-size: 1.5rem;
 	}
 </style>
